a6a03139c794949ba1c6ed8381cd078f94b7d709
egxn <afvelosas@correo.udistrital.edu.co>
2022-10-10 21:42:17 -0500

    📦 +Gnome light filter and time selection

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
 # Lizard 🦎
 
-🦎 A reminder to take eye breaks.
+🦎 A notification reminder in linux to take eye breaks.
 
 ## Usage
 
 ```bash
-cargo run
+  cargo run
 ```
 
+## Screenshots
+
+
+<img src="./assets/screenshot_1.png" width="240" alt="screenshot menu icon" />
+
+
+<img src="./assets/screenshot_0.png" width="240" alt="screenshot notification">
+
+
+
 ---
+
+🦀 🖤 🐧 
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use gtk::{self, traits::{MenuShellExt, GtkMenuItemExt, WidgetExt}};
+use gtk::{self, traits::{MenuShellExt, GtkMenuItemExt, WidgetExt, ContainerExt }, Label};
 use image::load_from_memory;
 use libappindicator::{AppIndicator, AppIndicatorStatus};
 use notify_rust::Notification;
-use rust_embed::{RustEmbed};
+use rust_embed::RustEmbed;
 use serde_derive::{Serialize, Deserialize};
-use std::{env, fs};
+use std::{env, fs, path::PathBuf};
+use std::process::Command;
 
 #[derive(RustEmbed)]
 #[folder = "assets/"]
@@ -19,45 +20,101 @@ impl ::std::default::Default for MyConfig {
     fn default() -> Self { Self { minutes: 20 } }
 }
 
-fn main() -> Result<(), confy::ConfyError> {
-    // confy::store("lizard", None, MyConfig { minutes: 1 })?;
-    let cfg: MyConfig = confy::load("lizard", None)?;    
+fn get_icon_path() -> (PathBuf, PathBuf) {
+    let dir = env::temp_dir().join("lizard");
+    let icon_path = dir.join("icon.png");
+    (dir, icon_path)
+}
 
-    gtk::init().unwrap();
-
-    let exit = gtk::MenuItem::with_label("Exit");
-    exit.connect_activate(move |_| { gtk::main_quit(); });
-
-    let mut menu = gtk::Menu::new();
-    menu.append(&exit);
-    menu.show_all();
-
-    let mut tray: AppIndicator = AppIndicator::new("lizard", "🧐");
-    tray.set_status(AppIndicatorStatus::Active);
-
-    let dir = env::temp_dir()
-        .join("lizard");
-
+fn copy_assets() -> () {
+    let (dir, icon_path) = get_icon_path();
     if !dir.exists() {
         fs::create_dir(&dir).unwrap();
     }
 
-    let icon_path = dir.join("icon.png");
     load_from_memory(&Asset::get("icon.png").unwrap().data)
         .unwrap()
         .save(&icon_path)
         .unwrap();
+}
 
+fn minutes_item(time: u64, icon: char) -> gtk::MenuItem {  
+    let minutes_box_item = gtk::MenuItem::new();
+    let time_label = Label::new(Some(&format!("{} {} minutes", icon, time)));
+
+    minutes_box_item.add(&time_label);
+    minutes_box_item.connect_activate(move |_| {
+        confy::store("lizard", None, MyConfig { minutes: time }).unwrap();
+    });
+
+    minutes_box_item
+}
+
+fn exit_item() -> gtk::MenuItem {
+    let item = gtk::MenuItem::with_label("🚪 Exit");
+    item.connect_activate(|_| gtk::main_quit());
+    item
+}
+
+fn turn_on_night_light() -> gtk::MenuItem {
+    let item = gtk::MenuItem::with_label("😎 Filter Light");
+    item.connect_activate(|_| {
+        Command::new("gsettings")
+            .args(&["set", "org.gnome.settings-daemon.plugins.color", "night-light-enabled", "true"])
+            .output()
+            .expect("failed to execute process");
+    });
+
+    item
+}
+
+fn turn_off_night_light() -> gtk::MenuItem {
+    let item = gtk::MenuItem::with_label("🤓 Normal Light");
+    item.connect_activate(|_| {        
+        Command::new("gsettings")
+            .args(&["set", "org.gnome.settings-daemon.plugins.color", "night-light-enabled", "false"])
+            .output()
+            .expect("failed to execute process");
+    });
+
+    item
+}
+
+fn create_tray_icon() -> () {
+    let (dir, _) = get_icon_path();
+    copy_assets();
+    let exit = exit_item();
+    let mut menu = gtk::Menu::new();
+    menu.append(&minutes_item(15, '⌛'));
+    menu.append(&minutes_item(20, '⌛'));
+    menu.append(&minutes_item(30, '⏳'));
+    menu.append(&gtk::SeparatorMenuItem::new());
+    menu.append(&turn_on_night_light());
+    menu.append(&turn_off_night_light());
+    menu.append(&gtk::SeparatorMenuItem::new());
+    menu.append(&exit);
+    menu.show_all();
+
+    let mut tray: AppIndicator = AppIndicator::new("lizard", "🧐");
+    tray.set_status(AppIndicatorStatus::Active);    
     tray.set_icon_theme_path(dir.to_str().unwrap());
     tray.set_icon_full("icon", "icon");
     tray.set_menu(&mut menu);
+}
+
+fn main() -> Result<(), confy::ConfyError> {
+    let cfg: MyConfig = confy::load("lizard", None)?;    
+
+    gtk::init().unwrap();
+    create_tray_icon();
+    let (_, icon_path) = get_icon_path();
 
     std::thread::spawn(move || {
         loop {
             std::thread::sleep(std::time::Duration::from_secs(cfg.minutes * 60));
             Notification::new()
                 .summary("Eyes break 🧐")
-                .body("look at something 6m away for at least 20 seconds")
+                .body("Look at something 6mt away for at least 20 seconds")
                 .icon(&icon_path.to_str().unwrap())
                 .urgency(notify_rust::Urgency::Critical)
                 .show()
@@ -68,7 +125,6 @@ fn main() -> Result<(), confy::ConfyError> {
                 });
         }
     });
-
 
     gtk::main();
     Ok(())
